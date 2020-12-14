157362b41c36ea681fd4e1c3a99358b9033c8a97
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-12-14 14:02:21 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+cout << "Welcome to the Party" << endl;
+
+cout << "Choose an Option (1-5)" << endl;
+
+
+
+
+
+return 0;
+}
