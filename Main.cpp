d901e051587058619ba283b9f85fef9439b15f85
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-12-29 03:09:51 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,12 +4,20 @@ using namespace std;
 
 int main(){
 
-cout << "Welcome to the Party" << endl;
+int currdate;
+char timewrap;
 
-cout << "Choose an Option (1-5)" << endl;
+cout << "Welcome to the Time Calculator" << endl;
 
+cout << "The current date is: " << endl;
+cout << currdate;
 
+cout << "Would you like to go forward (F) or backward (B) in time?" << endl;
+cin >> timewrap;
 
+if (timewrap == 'B' || timewrap == 'b'){
+cout << "Back to the future we go...But how far?" << endl;
+}
 
 
 return 0;
